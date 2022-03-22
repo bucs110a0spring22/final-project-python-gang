d5bc24496b57b8665a25316c80f2ca7b0703ef93
Christopher Micu <98533751+ChrisMicu@users.noreply.github.com>
2022-03-22 13:55:37 +0000

    Excercise in class

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import pygame
 #import your controller
 
+mylist = []
+
+for a in range(1,5):
+  number = int(input("Please enter a number: "))
+  mylist.append(number)
+
+print(*mylist, sep = "\n")
+
+def newlist(mylist):
+  number = len(newlist)
+  temp=newlist[0]
+  newlist[0]=newlist[number - 1]
+  newlist[number-1]= temp
+print(newlist)
+
+
+
+
 def main():
     pygame.init()
     #Create an instance on your controller object
@@ -11,3 +29,4 @@ def main():
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
+
